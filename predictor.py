e3aeb620789c6a5862320f5ba0801b2ca4260526
Taweeta-Sataporn <beataweeta.2405@gmail.com>
2024-06-08 00:35:53 +0700

    Test ka

---
+++ predictor.py
@@ -0,0 +1,14 @@
+# Import Libraries
+import pandas as pd
+import numpy as np
+import shap
+from IPython.display import display, HTML
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+import io
+from openpyxl import load_workbook, Workbook
+import tensorflow as tf
+from tensorflow import keras
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
